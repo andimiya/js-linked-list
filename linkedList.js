5a9a6dad47f35d9429e91c4702f7fc952c59c4eb
Andrea <takamiya.andrea@gmail.com>
2016-11-17 18:11:13 -1000

    Set up general structure, all tests passed up until the getTail method

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,51 @@
  */
 function linkedListGenerator(){
 
+  var head = null;
+
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    var currentNode = head;
+    return currentNode;
+  }
+
+  function add(Value) {
+    //create a
+    // var head =
+    // var newNodeA = {
+    //   value: Value,
+    //   next: null
+    // };
+  }
+
+  function get(Number){
+  }
+
+  function remove(Number) {
+  }
+
+  function insert(Value, Number) {
+  }
+
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    get: get,
+    remove: remove,
+    insert: insert
+  };
+
 }
+
+var ll = linkedListGenerator();
+// ll.add('kitten');
+// ll.add('puppy')
+// ll.add('lamb')
+// var newLinkedList = ll.getTail();
+// console.log(getHead(), 'getHead');
+// console.log(getTail(), 'getTail');
