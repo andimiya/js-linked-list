75a7e4dfa1ad71db59eda9d794089ff689b98ffe
Andrea <takamiya.andrea@gmail.com>
2016-11-19 12:20:40 -1000

    Added a function getLength to find the length of the list

--- linkedList.js
+++ linkedList.js
@@ -8,8 +8,9 @@ function linkedListGenerator(){
 
   function getTail() {
     let currentNode = head;
+    // console.log(currentNode, 'currentNode');
 
-    while (curentNode !== null){
+    while (currentNode !== null){
       if (currentNode.next !== null) {
         currentNode = currentNode.next;
       }
@@ -17,35 +18,91 @@ function linkedListGenerator(){
         return currentNode;
       }
     }
+
     return currentNode;
   }
 
   function add(newValue) {
+    let tail = getTail();
+
     let newNode = {
       value: newValue,
       next: null
     };
-    let tail = getTail();
-
+    // No liist exists, create new
     if(head === null) {
       head = newNode;
     }
-    if(head !== null){
-
+    // Add new node to end of existing list
+    else if(tail.next === null) {
+      tail.next = newNode;
     }
-
     return newNode;
   }
 
-  function remove() {
+  function remove(index) {
+    let previousNode = get(index-1);
+    let currentNode = get(index);
+    let nextNode = currentNode.next;
+
+    if (head === null) {
+      return false;
+    }
+    if (currentNode === false) {
+      return false;
+    }
+    if (index === 0) {
+      head = head.next;
+    }
+    else {
+      previousNode.next = nextNode;
+    }
 
   }
 
-  function get() {
+  function get(index) {
 
+    let currentNode = head;
+
+    for (var i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+      if (currentNode === null) {
+        return false;
+      }
+    }
+    return currentNode;
   }
 
-  function insert() {
+  function getLength() {
+    let length = 0;
+
+    while (node.next !== null) {
+      length++;
+    }
+    console.log(length, 'length');
+
+    return length;
+  }
+
+  function insert(value, index) {
+    let previousNode = get(index -1);
+    let currentNode = get(index);
+
+    let newNode = {
+      value: value,
+      index: index
+    };
+
+    if (index === 0) {
+      head = newNode;
+    }
+    else if (getLength() > index) {
+      return false;
+    }
+    else {
+      previousNode.next = newNode;
+      newNode.next = currentNode;
+    }
 
   }
 
@@ -56,5 +113,14 @@ function linkedListGenerator(){
     remove: remove,
     get: get,
     insert: insert
-  }
+  };
+
+
 }
+
+var ll = linkedListGenerator();
+console.log(ll.add(1));
+console.log(ll.add(2));
+console.log(ll.add(3));
+console.log(ll.add(4));
+
