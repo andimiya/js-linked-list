07396db36e69b9902e9eb7b8d473a94ac814a743
Andrea <takamiya.andrea@gmail.com>
2016-11-18 18:35:06 -1000

    Set up the returns and function holders

--- linkedList.js
+++ linkedList.js
@@ -1,2 +1,35 @@
 function linkedListGenerator(){
+
+  function getHead() {
+
+  }
+
+  function getTail() {
+
+  }
+
+  function add() {
+
+  }
+
+  function remove() {
+
+  }
+
+  function get() {
+
+  }
+
+  function insert() {
+
+  }
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert
+  }
 }
